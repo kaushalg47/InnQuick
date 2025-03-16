85ecfa60656b772e9d6812ac1f2dc590e7841349
Kaushal <92015842+kaushalg47@users.noreply.github.com>
2025-03-17 00:22:21 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --noinput
 python manage.py migrate
-python manage.py createsuperuser --no-input --email "admin@email.com"
+python manage.py createsuperuser --no-input --username "admin" --email "admin@email.com"
